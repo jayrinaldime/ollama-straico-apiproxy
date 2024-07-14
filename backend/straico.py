dad2b957e239eec4bca298bebd67037ab35e97cc
JRinaldi <jrinaldi@jayrinaldi.me>
2024-07-14 20:10:51 +0900

    create a dev docker image

--- backend/straico.py
+++ backend/straico.py
@@ -7,10 +7,12 @@ logger = logging.getLogger(__name__)
 STRAICO_API_KEY = environ["STRAICO_API_KEY"]
 logger.info(f"STRAICO_API_KEY={STRAICO_API_KEY}")
 def prompt_completion(msg: str, model: str= "openai/gpt-3.5-turbo-0125") -> str:
+    post_request_data = {"model": model,
+                      "message": msg}
+    logger.debug(f"Request Post Data: {post_request_data}")
     r = requests.post("https://api.straico.com/v0/prompt/completion",
               headers = {"Authorization": f"Bearer {STRAICO_API_KEY}"},
-              data = {"model": model,
-                      "message": msg})
+              data = post_request_data)
     logger.debug(f"status code: {r.status_code}")
     logger.debug(f"response body: {r.content}")
     return r.json()["data"]["completion"]["choices"][-1]["message"]["content"]
