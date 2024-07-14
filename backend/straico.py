4a0fea776558012cd1183c36cd5b75fc9a0d73b4
JRinaldi <jrinaldi@jayrinaldi.me>
2024-07-14 20:18:16 +0900

    remove latest in model

--- backend/straico.py
+++ backend/straico.py
@@ -7,6 +7,9 @@ logger = logging.getLogger(__name__)
 STRAICO_API_KEY = environ["STRAICO_API_KEY"]
 logger.info(f"STRAICO_API_KEY={STRAICO_API_KEY}")
 def prompt_completion(msg: str, model: str= "openai/gpt-3.5-turbo-0125") -> str:
+    # some  clients add :latest
+    if model.endswith(":latest"):
+        model = model.replace(":latest","")
     post_request_data = {"model": model,
                       "message": msg}
     logger.debug(f"Request Post Data: {post_request_data}")
