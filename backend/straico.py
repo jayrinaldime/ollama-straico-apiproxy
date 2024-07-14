6e50e82bc8a5c70d2846be72e9aee906d9f0cb6f
JRinaldi <jrinaldi@jayrinaldi.me>
2024-07-14 19:47:12 +0900

    add debugging of apikey

--- backend/straico.py
+++ backend/straico.py
@@ -1,7 +1,11 @@
 import requests
 from os import environ
-STRAICO_API_KEY = environ["STRAICO_API_KEY"]
+from app import logging
 
+logger = logging.getLogger(__name__)
+
+STRAICO_API_KEY = environ["STRAICO_API_KEY"]
+logger.info(f"STRAICO_API_KEY={STRAICO_API_KEY}")
 def prompt_completion(msg: str, model: str= "openai/gpt-3.5-turbo-0125") -> str:
     r = requests.post("https://api.straico.com/v0/prompt/completion",
               headers = {"Authorization": f"Bearer {STRAICO_API_KEY}"},
