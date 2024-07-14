c557969c83f30f577928938981fb210c00a1bc97
JRinaldi <jrinaldi@jayrinaldi.me>
2024-07-15 00:32:36 +0900

    updated log

--- backend/__init__.py
+++ backend/__init__.py
@@ -8,7 +8,7 @@ logger.debug(f"STRAICO_API_KEY={STRAICO_API_KEY}")
 
 if STRAICO_API_KEY.strip().upper() == "TEST":
     from .test import prompt_completion, list_model
-    logger.info("Using Backend Test")
+    logger.info("Using Test Backend")
 else:
     from .straico import prompt_completion, list_model
-    logger.info("Using Backend Straico")
+    logger.info("Using Straico Backend")
