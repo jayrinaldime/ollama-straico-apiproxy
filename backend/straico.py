b9e945f8a9038f2f17867ffa76fe8d26483156c3
JRinaldi <jrinaldi@jayrinaldi.me>
2024-07-14 22:39:58 +0900

    Squashed commit of the following:
    commit 1684497e7f820ce613f14cbaf362c6a3d47e7e68
    Author: JRinaldi <jrinaldi@jayrinaldi.me>
    Date:   Sun Jul 14 22:39:03 2024 +0900
    
        Add unique user agent to Straico API Request
    
        Added easy way to identify HTTP request coming from API proxy.

--- backend/straico.py
+++ backend/straico.py
@@ -1,11 +1,17 @@
 import requests
 from os import environ
 from app import logging
+from const import VERSION, PROJECT_NAME
+import platform
 
 logger = logging.getLogger(__name__)
 
 STRAICO_API_KEY = environ["STRAICO_API_KEY"]
 logger.info(f"STRAICO_API_KEY={STRAICO_API_KEY}")
+
+CLIENT_USER_AGENT = f"{PROJECT_NAME}/{VERSION} ({platform.system()}; {platform.processor()};) py/{platform.python_version()}"
+logger.debug(f"Straico Client User Agent set = f{CLIENT_USER_AGENT}")
+
 def prompt_completion(msg: str, model: str= "openai/gpt-3.5-turbo-0125") -> str:
     # some  clients add :latest
     if model.endswith(":latest"):
@@ -14,7 +20,8 @@ def prompt_completion(msg: str, model: str= "openai/gpt-3.5-turbo-0125") -> str:
                       "message": msg}
     logger.debug(f"Request Post Data: {post_request_data}")
     r = requests.post("https://api.straico.com/v0/prompt/completion",
-              headers = {"Authorization": f"Bearer {STRAICO_API_KEY}"},
+              headers = {"Authorization": f"Bearer {STRAICO_API_KEY}",
+                         "User-Agent": CLIENT_USER_AGENT},
               data = post_request_data)
     logger.debug(f"status code: {r.status_code}")
     logger.debug(f"response body: {r.content}")
