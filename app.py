e934e1cb287c50c13d3373f978dece1eec94b0df
JRinaldi <jrinaldi@jayrinaldi.me>
2024-10-05 16:16:46 +0900

    add logging to feature flags at app init

--- app.py
+++ app.py
@@ -10,14 +10,17 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S",
 )
+logger = logging.getLogger(__name__)
 
 app = FastAPI()
 
-
 EMBEDDING_ENABLED = os.environ.get("EMBEDDING_ENABLED", "false").lower().strip()
 EMBEDDING_ENABLED = EMBEDDING_ENABLED not in ["0", "false", "no"]
+logger.info(f"Embedding Enabled: {EMBEDDING_ENABLED}")
 
 TRANSCRIPTION_ENABLED = os.environ.get("TRANSCRIPTION_ENABLED", "false").lower().strip()
 TRANSCRIPTION_ENABLED = TRANSCRIPTION_ENABLED not in ["0", "false", "no"]
+logger.info(f"Speech to Text Enabled: {TRANSCRIPTION_ENABLED}")
 
 PLATFORM_ENABLED = os.environ.get("STRAICO_PLATFORM_ACCESS_TOKEN") is not None
+logger.info(f"Platform Enabled: {PLATFORM_ENABLED}")
