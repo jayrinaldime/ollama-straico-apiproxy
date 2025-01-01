8a9f64b766d881474d7d90a356b1520a64e476b1
JRinaldi <jrinaldi@jayrinaldi.me>
2025-01-01 20:37:56 +0900

    fix logging issue with chat completion

--- api_endpoints/lm_studio/chat.py
+++ api_endpoints/lm_studio/chat.py
@@ -32,7 +32,7 @@ def _get_msg_image(content):
 
 @app.post("/chat/completions")
 @app.post("/v1/chat/completions")
-#@observe
+@observe
 async def chat_completions(request: Request):
     try:
         post_json_data = await request.json()
