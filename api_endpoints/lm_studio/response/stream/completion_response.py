75cc2d617124366af12812860031697c02401123
JRinaldi <jrinaldi@jayrinaldi.me>
2025-05-30 02:32:29 +0900

    add stream tool calling for openai
    sometimes still fails

--- api_endpoints/lm_studio/response/stream/completion_response.py
+++ api_endpoints/lm_studio/response/stream/completion_response.py
@@ -9,14 +9,57 @@ def stream_data_response(msg):
     return "data: " + json.dumps(msg, ensure_ascii=False) + "\n\n"
 
 
-def end_response(rid, model):
+def end_response(rid, model, finish_reason="stop"):
     return {
         "id": f"chatcmpl-{rid}",
         "object": "chat.completion.chunk",
         "created": 1716456766,
         "model": model,
-        "choices": [{"index": 0, "delta": {}, "finish_reason": "stop"}],
+        "system_fingerprint": model,
+        "choices": [{"index": 0, "delta": {}, "finish_reason": finish_reason}],
     }
+
+def streamed_response_toolcall(tool_call, model):
+    request_id = str(uuid.uuid4())
+    tool_call["tool_calls"][0]["index"]=0
+
+    yield stream_data_response(
+        {
+            "id": f"chatcmpl-{request_id}",
+            "object": "chat.completion.chunk",
+            "created": 1748531779,
+            "model": model,
+            "system_fingerprint": model,
+            "choices": [
+                {
+                    "index": 0,
+                    "delta": {"role":"assistant","content": "\n\n"},
+                    "finish_reason": None,
+                }
+            ],
+        }
+    )
+    yield stream_data_response(
+        {
+            "id": f"chatcmpl-{request_id}",
+            "object": "chat.completion.chunk",
+            "created": 1748531779,
+            "model": model,
+            "system_fingerprint": model,
+            "choices": [
+                {
+                    "index": 0,
+                    "delta": {"tool_calls": tool_call["tool_calls"]},
+                    "finish_reason": None,
+                    "logprobs": None,
+                }
+            ],
+        }
+    )
+
+    yield stream_data_response(end_response(request_id, model, finish_reason="tool_calls"))
+
+    yield "data: [DONE]\n\n"
 
 
 def streamed_response(response, model):
@@ -29,6 +72,7 @@ def streamed_response(response, model):
             "object": "chat.completion.chunk",
             "created": 1716456766,
             "model": model,
+            "system_fingerprint": model,
             "choices": [
                 {
                     "index": 0,
---
+++ test/openai_function_calling_stream.py
@@ -0,0 +1,36 @@
+from openai import OpenAI, Client
+
+# Set your OpenAI API key
+api_key = "skkkkkk"
+
+# Set a custom base URL (optional)
+
+client = Client(base_url="http://127.0.0.1:3214/v1", api_key=api_key)
+# Define the conversation
+
+tools = [
+    {
+        "type": "function",
+        "function": {
+            "name": "get_weather",
+            "parameters": {
+                "type": "object",
+                "properties": {"location": {"type": "string"}},
+            },
+        },
+    }
+]
+
+completion = client.chat.completions.create(
+    model="openai/gpt-4o-mini", #,"qwen/qwen3-1.7b",#"openai/gpt-4o-mini",
+    messages=[{"role": "user", "content": "What's the weather like in Paris today?"}],
+    tools=tools,
+    stream=True
+)
+import pprint
+for s in completion:
+    print(s)
+"""
+ChatCompletionChunk(id='chatcmpl-ib7umc0agyl0vwusbisn', choices=[Choice(delta=ChoiceDelta(content=None, function_call=None, refusal=None, role=None, tool_calls=[ChoiceDeltaToolCall(index=0, id=None, function=ChoiceDeltaToolCallFunction(arguments='"}', name=None), type='function')]), finish_reason=None, index=0, logprobs=None)], created=1748530158, model='qwen/qwen3-1.7b', object='chat.completion.chunk', service_tier=None, system_fingerprint='qwen/qwen3-1.7b', usage=None)
+ChatCompletionChunk(id='chatcmpl-ib7umc0agyl0vwusbisn', choices=[Choice(delta=ChoiceDelta(content=None, function_call=None, refusal=None, role=None, tool_calls=None), finish_reason='tool_calls', index=0, logprobs=None)], created=1748530158, model='qwen/qwen3-1.7b', object='chat.completion.chunk', service_tier=None, system_fingerprint='qwen/qwen3-1.7b', usage=None)
+"""
